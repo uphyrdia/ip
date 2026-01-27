afb2cbad8f29e53d32361540a58d3edf7dfdd88d
uphyrdia <uphyrdia@gmail.com>
2026-01-28 07:52:36 +0800

    Add increment Level-2

--- src/main/java/Phrolova.java
+++ src/main/java/Phrolova.java
@@ -4,12 +4,29 @@ public class Phrolova {
     public static void main(String[] args) {
         // System.out.println("\tCiallo ～(∠・ω< )⌒★!");
         System.out.println("\tI'm Phrolova.\n\tWhat can I do for you?");
+        String[] tasks = new String[100];
         Scanner in = new Scanner(System.in);
-        String message = in.nextLine();;
-        while (!message.equals("bye")) {
-            System.out.println("\t" + message);
+        String message;
+        int i = 0;
+        while (true) {
             message = in.nextLine();
+            if (message.equals("bye")) {
+                System.out.println("\tBye.");
+                return;
+            }
+            if (message.equals("list")) {
+                for (int j = 0; j < i; j++) {
+                    System.out.println("\t" + (j+1) + ". " + tasks[j]);
+                }
+                continue;
+            }
+            if (i == 100) {
+                System.out.println("\tHow can you have so many things to do? Go relax.");
+                return;
+            }
+            tasks[i] = message;
+            System.out.println("\tadded: " + tasks[i]);
+            i++;
         }
-        System.out.println("\tBye.");
     }
 }
