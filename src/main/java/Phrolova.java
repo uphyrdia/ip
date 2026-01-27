2b97966e32a85e281b3f8e2cbb091d317644d907
uphyrdia <uphyrdia@gmail.com>
2026-01-28 07:25:35 +0800

    Add increment Level-1

--- src/main/java/Phrolova.java
+++ src/main/java/Phrolova.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class Phrolova {
     public static void main(String[] args) {
-        System.out.println("Hello! I'm Phrolova.\nWhat can I do for you?");
-        System.out.println("\nBye. Hope to see you again soon!");
+        // System.out.println("\tCiallo ～(∠・ω< )⌒★!");
+        System.out.println("\tI'm Phrolova.\n\tWhat can I do for you?");
+        Scanner in = new Scanner(System.in);
+        String message = in.nextLine();;
+        while (!message.equals("bye")) {
+            System.out.println("\t" + message);
+            message = in.nextLine();
+        }
+        System.out.println("\tBye.");
     }
 }
