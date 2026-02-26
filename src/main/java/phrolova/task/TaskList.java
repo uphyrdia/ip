042ac75c3af19a10cd6cf7e1364553132c7fc9db
uphyrdia <uphyrdia@gmail.com>
2026-02-26 09:15:53 +0800

    Add class Storage

--- src/main/java/phrolova/task/TaskList.java
+++ src/main/java/phrolova/task/TaskList.java
@@ -1,63 +1,20 @@
 package phrolova.task;
 
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 import java.util.ArrayList;
-import java.nio.file.Paths;
-import java.util.List;
 
 import phrolova.exception.*;
+import phrolova.storage.Storage;
 import phrolova.ui.UI;
 
 public class TaskList {
 
     private final ArrayList<Task> tasks = new ArrayList<>();
-    private final Path savePath = Paths.get("data","Phrolova.txt");
-    UI ui = new UI();
-
-    public void save() throws IOException {
-        Files.createDirectories(savePath.getParent());
-        List<String> lines = new ArrayList<>();
-        for (Task t : tasks) {
-            lines.add(t.toText());
-        }
-        Files.write(savePath, lines);
-    }
+    private final Storage storage = new Storage(tasks);
+    private final UI ui = new UI();
 
     public void load() throws IOException {
-        if (!Files.exists(savePath)) {
-            return;
-        }
-        List<String> lines = Files.readAllLines(savePath);
-        for (String line : lines) {
-            try {
-                tasks.add(parseTask(line));
-            } catch (Exception e) {
-                System.out.println("Warning: Skipping corrupted line -> " + line);
-            }
-        }
-    }
-
-    public Task parseTask(String line) {
-        String[] parts = line.split("\\|");
-
-        String type = parts[0];
-        boolean isDone = parts[1].equals("1");
-        String description = parts[2];
-
-        Task task = switch (type) {
-            case "T" -> new Todo(description);
-            case "D" -> new Deadline(description, parts[3]);
-            case "E" -> new Event(description, parts[3], parts[4]);
-            default -> throw new IllegalArgumentException("Invalid task type");
-        };
-
-        if (isDone) {
-            task.markWithoutMessage();
-        }
-
-        return task;
+        storage.load();
     }
 
     public void add(Task task) {
@@ -81,7 +38,7 @@ public class TaskList {
         } catch (Exception e) {
             ui.print("Invalid command.");
         }
-        save();
+        storage.save();
     }
 
     public void mark(String message) throws IOException {
@@ -91,7 +48,7 @@ public class TaskList {
         } catch (Exception e) {
             ui.print("Invalid command.");
         }
-        save();
+        storage.save();
     }
 
     public void unmark(String message) throws IOException {
@@ -101,7 +58,7 @@ public class TaskList {
         } catch (Exception e) {
             ui.print("Invalid command.");
         }
-        save();
+        storage.save();
     }
 
     public void addTodo(String message) throws MissingTaskException, IOException {
@@ -109,7 +66,7 @@ public class TaskList {
             throw new MissingTaskException();
         }
         add(new Todo(message.substring(5)));
-        save();
+        storage.save();
     }
 
     public void addDeadline(String message) throws IOException {
@@ -119,7 +76,7 @@ public class TaskList {
         } catch (Exception e) {
             ui.print("Invalid command.");
         }
-        save();
+        storage.save();
     }
 
     public void addEvent(String message) throws IOException {
@@ -130,7 +87,7 @@ public class TaskList {
         } catch (Exception e) {
             ui.print("Invalid command.");
         }
-        save();
+        storage.save();
     }
 
 }
